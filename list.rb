6c39be1ba77433d3f0b698d6a6c5b9a88dcd7921
JoanAlvarado040700 <sevaslarios@gmail.com>
2022-10-15 23:01:59 -0600

    Clases Item y list

---
+++ list.rb
@@ -0,0 +1,36 @@
+class List
+    attr_writer :items
+
+    def initialize()
+        @items = Arraty.new()
+    end
+
+    def add_items(text)
+        item = Item.new(text)
+        @items.push(item)
+    end
+
+    def remove_items(index)
+        @items.delete(index)
+    end    
+
+    def check(index)
+        @items[index].check = true
+    end
+    
+    def remove_all
+        @items.clear
+    end
+
+    def show_all
+        if @items.length == 0
+            pust "No hay ningun articulo en tu lista!"
+        else
+            @items.each_index do |index|
+                pust index.to_s + " - "+ @items[index].to_s
+            end
+        end
+    end
+    
+    
+end
